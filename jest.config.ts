5d92b61404cc91f0bee8e0b93ea3fe532b719a8e
Lucas Bastos <lucsbasto@gmail.com>
2022-01-06 10:04:32 -0300

    chore: initial config

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
