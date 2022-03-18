7ddfdb499e3dca0282b85d98f65bf8c798085066
Lucas Bastos <lucsbasto@gmail.com>
2022-03-18 12:38:25 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, Collection } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -7,5 +7,8 @@ export const MongoHelper = {
   },
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+  async getCollection (name: string): Promise<Collection> {
+    return this.client.db().collection(name)
   }
 }
