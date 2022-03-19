3800551286c024eeba5b6e70fc61a6d3d653679c
Lucas Bastos <lucsbasto@gmail.com>
2022-03-18 23:03:22 -0300

    feat: ensure requests parse body as json

---
+++ src/main/config/app.ts
@@ -0,0 +1,6 @@
+import express from 'express'
+import setupMiddleware from './middlewares'
+
+const app = express()
+setupMiddleware(app)
+export default app
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,2 @@
-import express from 'express'
-
-const app = express()
+import app from './config/app'
 app.listen(5050, () => console.log('Server listening on port 5050'))
