d2feb1a8c9ae2735c0cc478f65edd26274325c6b
Lucas Bastos <lucsbasto@gmail.com>
2022-03-23 11:40:42 -0300

    test: add tests to CompareFieldsValidation

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,17 @@
+import { InvalidParamError } from '../../../presentation/errors'
+import { CompareFieldsValidation } from './compare-fields-validation'
+const makeSut = (): CompareFieldsValidation => {
+  return new CompareFieldsValidation('field', 'fieldToCompare')
+}
+describe('CompareFields Validation', () => {
+  test('Should return a InvalidParamError if validation fails', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 'any_value', fieldToCompare: 'wrong_value' })
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+  })
+  test('Should not return if validation succeeds', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 'any_value', fieldToCompare: 'any_value' })
+    expect(error).toBeFalsy()
+  })
+})
