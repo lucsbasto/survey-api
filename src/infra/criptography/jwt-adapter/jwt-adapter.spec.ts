0046d82a3365d649aa0c030c3a5b1cd0ffda2413
Lucas Bastos <lucsbasto@gmail.com>
2022-04-03 21:23:02 -0300

    test: ensure JwtAdapter throws if sign throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -18,4 +18,10 @@ describe('Jwt Adapter', () => {
     const accessToken = await sut.encrypt('any_value')
     expect(accessToken).toBe('any_token')
   })
+  test('Should throw if sign throws', async () => {
+    const sut = new JwtAdapter('secret')
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
+  })
 })
