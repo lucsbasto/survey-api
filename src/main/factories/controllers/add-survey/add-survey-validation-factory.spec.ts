a1c71a675298f2a13a907437363140d58e446448
Lucas Bastos <lucsbasto@gmail.com>
2022-04-08 20:16:48 -0300

    test: ensure AddSurveyValidation calls ValidationComposite with all validations

---
+++ src/main/factories/controllers/add-survey/add-survey-validation-factory.spec.ts
@@ -0,0 +1,16 @@
+import { ValidationComposite, RequiredFieldValidation } from '../../../../validation/validators'
+import { makeAddSurveyValidation } from './add-survey-validation-factory'
+import { Validation } from '../../../../presentation/protocols'
+
+jest.mock('../../../../validation/validators/validator-composite')
+
+describe('AddSurveyValidation Factory', () => {
+  test('Should call ValidationComposite with all validations', () => {
+    makeAddSurveyValidation()
+    const validations: Validation[] = []
+    for (const field of ['question', 'answers']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
