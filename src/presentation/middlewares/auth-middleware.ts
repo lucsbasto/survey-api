6ea2bd25ed047efed05d0e223df0c480ca8ec7c2
Lucas Bastos <lucsbasto@gmail.com>
2022-04-08 21:04:30 -0300

    feat: ensure AuthMiddleware returns 403 if no x-access-token exists in headers

---
+++ src/presentation/middlewares/auth-middleware.ts
@@ -0,0 +1,9 @@
+import { AccessDeniedError } from '../errors'
+import { forbidden } from '../helpers/http/http-helper'
+import { Middleware, HttpRequest, HttpResponse } from '../protocols'
+
+export class AuthMiddleware implements Middleware {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    return forbidden(new AccessDeniedError())
+  }
+}
