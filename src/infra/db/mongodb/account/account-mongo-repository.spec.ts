4cf43352b2326723083e9e3572bdac421f9f9e9a
Lucas Bastos <lucsbasto@gmail.com>
2022-04-09 15:20:53 -0300

    test: ensure AccountMongoRepository returns an account on loadByToken without role success

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -70,4 +70,21 @@ describe('Account Mongo Repository', () => {
       expect(fakeAccountWithToken.accessToken).toBe('any_token')
     })
   })
+  describe('loadByToken()', () => {
+    test('Should return an account on loadByToken without role success', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token'
+      })
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
+    })
+  })
 })
