da86c1ca8ab74f3637d43d273f99edbb9a51a115
Lucas Bastos <lucsbasto@gmail.com>
2022-04-11 16:28:38 -0300

    test: ensure DbLoadSurveys return a list of surveys on success

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -49,4 +49,9 @@ describe('DbLoadSurveys', () => {
     await sut.load()
     expect(loadSpy).toBeCalled()
   })
+  test('should return a list of surveys on success', async () => {
+    const { sut } = makeSut()
+    const surveys = await sut.load()
+    expect(surveys).toEqual(makeFakeSurveys())
+  })
 })
