1498623cc6d54301ab2e21d21b340a24a9cbfa0d
Lucas Bastos <lucsbasto@gmail.com>
2022-04-23 12:05:12 -0300

    test: ensure save survey result route returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -1,6 +1,30 @@
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import request from 'supertest'
 import app from '../config/app'
+import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import env from '@/main/config/env'
+
+let surveyCollection: Collection
+let accountCollection: Collection
+
+const makeUserAccessToken = async (): Promise<string> => {
+  const res = await accountCollection.insertOne({
+    name: 'user',
+    email: 'user@mail.com',
+    password: '123'
+  })
+  const id = res.insertedId.toHexString()
+  const accessToken = sign({ id }, env.jwtSecret)
+  await accountCollection.updateOne({
+    _id: res.insertedId
+  }, {
+    $set: {
+      accessToken
+    }
+  })
+  return accessToken
+}
 
 describe('Survey Result Routes', () => {
   beforeAll(async () => {
@@ -8,6 +32,12 @@ describe('Survey Result Routes', () => {
   })
   afterAll(async () => {
     await MongoHelper.disconnect()
+  })
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await surveyCollection.deleteMany({})
+    await accountCollection.deleteMany({})
   })
   describe('PUT /surveys/:surveyId/results', () => {
     test('Should return 403 on save survey result without accessToken', async () => {
@@ -17,6 +47,27 @@ describe('Survey Result Routes', () => {
           answer: 'any_answer'
         })
         .expect(403)
+    })
+    test('Should return 200 on save survey result with valid accessToken', async () => {
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          image: 'http://image-name.com',
+          answer: 'Answer 1'
+        },
+        {
+          image: 'http://image2-name.com',
+          answer: 'Answer 2'
+        }]
+      })
+      const accessToken = await makeUserAccessToken()
+      await request(app)
+        .put(`/api/surveys/${res.ops[0]._id}/results`)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'Answer 1'
+        })
+        .expect(200)
     })
   })
 })
