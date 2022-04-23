7ef1fb915b4a7089d2a661930bc4fcbd285bcf87
Lucas Bastos <lucsbasto@gmail.com>
2022-04-22 23:27:16 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResult with correct values

--- src/presentation/controllers/survey-result/save-survey-result-controller-protocols.ts
+++ src/presentation/controllers/survey-result/save-survey-result-controller-protocols.ts
@@ -1,3 +1,5 @@
 export * from '@/domain/usecases/survey/load-survey-by-id'
 export * from '@/presentation/protocols'
 export * from '../survey/load-surveys/load-surveys-protocols'
+export * from '@/domain/usecases/survey-result/save-survey-result'
+export * from '@/domain/models/survey-result'
--- src/presentation/controllers/survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result-controller.ts
@@ -1,19 +1,30 @@
 import { InvalidParamError } from '../../errors'
 import { forbidden, serverError } from '../../helpers/http/http-helper'
-import { Controller, LoadSurveyById } from './save-survey-result-controller-protocols'
+import { Controller, HttpRequest, LoadSurveyById, SaveSurveyResult } from './save-survey-result-controller-protocols'
 
 export class SaveSurveyResultController implements Controller {
-  constructor (private readonly loadSurveyById: LoadSurveyById) {}
-  async handle (httpRequest: any): Promise<any> {
+  constructor (
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly saveSurveyResult: SaveSurveyResult
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<any> {
     try {
       const { surveyId } = httpRequest.params
-      const { answer } = httpRequest.body
+      const { accountId } = httpRequest
+      const { answer, date } = httpRequest.body
       const survey = await this.loadSurveyById.loadById(surveyId)
       if (survey) {
         const answers = survey.answers.map(a => a.answer)
         if (!answers.includes(answer)) {
           return forbidden(new InvalidParamError('answer'))
         }
+        await this.saveSurveyResult.save({
+          accountId,
+          surveyId,
+          answer,
+          date
+        })
       } else {
         return forbidden(new InvalidParamError('surveyId'))
       }
