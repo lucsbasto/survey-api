7e74b33e76f43ade3ed023886bc4efb7784a0d08
Lucas Bastos <lucsbasto@gmail.com>
2022-04-26 09:58:00 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,25 @@
+import { loginPath } from './paths/login-path'
+import { accountSchema } from './schemas/account-schema'
+import { loginParamsSchema } from './schemas/login-params-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'API',
     description: 'API do Curso do Manguinho de NodeJs, Typescript, TDD, Clean Architecture e SOLID',
     version: '1.0.0'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [
+    { name: 'Login' }
+  ],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
